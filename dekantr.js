a77b7c091d4013e7ee52282a0b406203281cd4a6
Tyler Seabury <tylerseabury@gmail.com>
2016-10-14 01:49:53 +0000

    added basic methods: fetch(), dekant()

--- dekantr.js
+++ dekantr.js
@@ -6,11 +6,21 @@ var dekantr = {
     
     buffer: null,
     
-    html: null, // root DOM element to hold in dekantr object for easy reference, this could really be anything, i.e. the best way to hold access the document
+    html: null, // root DOM element to hold in dekantr object for easy reference, this could really be anything, i.e. the best way to access the document
     
-    fetch: function() { // build the XMLHttprequest handler here. This should take either a string or url ( or build a url based on a string, or know module name)
+    modules: {}, 
+    
+    fetch: function(url) { // build the XMLHttprequest handler here. This should take either a string or url ( or build a url based on a string, or know module name)
         
+        var r = new XMLHttpRequest();
         
+        r.responseType = "document";
+        
+        r.open("post", url);
+        
+        r.send();
+        
+        return r;
         
     },
     

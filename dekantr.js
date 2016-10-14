1b9e10f4ae0d7cee3ce0a894339f98bf3b52ede6
Tyler Seabury <tylerseabury@gmail.com>
2016-10-14 01:44:57 +0000

    creation of dekantr.js and developing pseodo code to get bearings

---
+++ dekantr.js
@@ -0,0 +1,23 @@
+
+
+
+
+var dekantr = {
+    
+    buffer: null,
+    
+    html: null, // root DOM element to hold in dekantr object for easy reference, this could really be anything, i.e. the best way to hold access the document
+    
+    fetch: function() { // build the XMLHttprequest handler here. This should take either a string or url ( or build a url based on a string, or know module name)
+        
+        
+        
+    },
+    
+    dekant: function(targetNode) { // this will be the method that "dekants" the retrieved and build modular content into the site's DOM
+        
+        
+        
+    }
+    
+}

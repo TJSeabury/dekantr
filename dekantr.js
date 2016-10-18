be0c57ebf4204cc2bbed6a62353d68d3c41a1871
Tyler Seabury <tylerseabury@gmail.com>
2016-10-18 14:22:19 +0000

    Gutted fetch() for the time being; It will be used for a different purpose latter. Fleshed out dekant and got basic behavior functioning.

--- dekantr.js
+++ dekantr.js
@@ -1,33 +1,57 @@
 
-
-
-
-var dekantr = {
+function dekantrPrime() {
+    "use strict";
     
-    buffer: null,
+    this.buffer = null;
     
-    html: null, // root DOM element to hold in dekantr object for easy reference, this could really be anything, i.e. the best way to access the document
+    this.html = null; // root DOM element to hold in dekantr object for easy reference, this could really be anything, i.e. the best way to access the document
     
-    modules: {}, 
+    this.modules = {}; 
     
-    fetch: function(url) { // build the XMLHttprequest handler here. This should take either a string or url ( or build a url based on a string, or know module name)
+    this.fetch = function(url) { // build the XMLHttprequest handler here. This should take either a string or url ( or build a url based on a string, or know module name)
         
-        var r = new XMLHttpRequest();
+        
+        
+    };
+    
+    this.dekant = function(targetNode) { // this will be the method that "dekants" the retrieved and build modular content into the site's DOM
+        
+        targetNode = document.getElementById(targetNode);
+        
+        let url = targetNode.getAttribute("data-dekantr-module") + ".html",
+        
+        r = new XMLHttpRequest();
         
         r.responseType = "document";
         
-        r.open("post", url);
+        r.overrideMimeType('text/xml');
         
-        r.send();
+        r.open("get", url);
         
-        return r;
+        r.onload = function () {
+            
+            if (r.readyState === r.DONE) {
+                
+                if (r.status === 200) {
+                    
+                    console.log(r);
+                    
+                    var injectable = r.responseXML.getElementsByTagName("body")[0].innerHTML;
+                    
+                    console.log(injectable);
+                    
+                    targetNode.innerHTML = injectable;
+                    
+                }
+                
+            }
+            
+        };
         
-    },
-    
-    dekant: function(targetNode) { // this will be the method that "dekants" the retrieved and build modular content into the site's DOM
-        
-        
-        
-    }
+        r.send(null);
+
+    };
     
 }
+
+var dekantr = new dekantrPrime();
